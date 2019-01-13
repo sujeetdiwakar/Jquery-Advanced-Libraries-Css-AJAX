43ba6cb3cb65e007aa2b27c10268ec3afd0d8cd8
Sujeet Kumar <33665990+sujeetdiwakar@users.noreply.github.com>
2019-01-13 22:26:12 +0530

    Date Picker Jquery ui

---
+++ 3-date-picker/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Date Picker</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" rel="stylesheet">
+    <script>
+        $(function () {
+           $('#date').datepicker({dateFormat:'yy-mm-dd'});
+           $('#date').datepicker('show');
+        });
+    </script>
+</head>
+<body>
+<p>
+    <input type="text" id="date">
+</p>
+</body>
+</html>
